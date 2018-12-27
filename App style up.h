f930d840394d1a540e84b73abe3580a647a5fc2b
Maciek1200 <46198848+Maciek1200@users.noreply.github.com>
2018-12-27 14:41:54 -0500

    Add files via upload

---
+++ App style up.h
@@ -0,0 +1,19 @@
+//
+//  App style up.h
+//  App style up
+//
+//  Created by Maciek Popielarz on 8/13/15.
+//  Copyright (c) 2015 Maciek Popielarz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for App style up.
+FOUNDATION_EXPORT double App_style_upVersionNumber;
+
+//! Project version string for App style up.
+FOUNDATION_EXPORT const unsigned char App_style_upVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <App_style_up/PublicHeader.h>
+
+
